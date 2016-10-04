9da66e35cdfee6fcea45168dfc941e26abf16ea0
Achrine <achrine1@gmail.com>
2016-10-04 16:52:56 -0700

    Create main.cpp
    added shit

---
+++ Ch-3-3/main.cpp
@@ -0,0 +1,23 @@
+//Programmer: Steven Brcak 
+//Assignment: Programming Challenge Chapter 3 Number 3:
+// 
+//Filename: main.cpp
+//Creation date: October 4, 2016
+
+//Preprocessor Directive. 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//function(module) main.
+//All c++ begins with main. 
+
+int main(){
+  int test01,
+      test02,
+      test03,
+      test04,
+      test05;
+  double average; 
+//Overview Statement 
+  cout << "This program will calculate the average of five test scores.\n\n";
