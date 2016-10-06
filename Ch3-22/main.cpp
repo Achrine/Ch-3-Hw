fe5413f6249a6706b8cd69eaf6979138a6b081be
mvc <mvc>
2016-10-06 15:14:30 -0700

    This is the required work

---
+++ Ch3-22/main.cpp
@@ -0,0 +1,14 @@
+//Programmer: Steven 
+// Assignment: Ch3 Challenge 1
+//Filename: main.cpp
+//Creation Date: 10/06/16
+//Description: The purpose of this program is to calculate gas mileage for a car. 
+//Book description: Write a program that calculates a car's gas mileage. The program should ask for. 
+// The number of gallons of gas the car can hold, the number of miles driven on a full tank. 
+// It should display the number of miles driven per gallon. Or miles per gallon. 
+
+//Ch3, challenge 1, Miles per Gallon. 
+
+//Preprocessor Directive 
+#include <iostream>$
+using namespace std;
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Ch-3-Hw
 Ch 3 Hw for Gaddis Starting out with C++
+
+
+Problems 1,3,4 and 10 are already done. Go bottom up
