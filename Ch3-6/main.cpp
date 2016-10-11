224aef8fdc336dc280abd4352229bb010e6698f1
mvc <mvc>
2016-10-11 15:50:39 -0700

    Challenge 6

--- Ch3-6/main.cpp
+++ Ch3-6/main.cpp
@@ -1,14 +1,35 @@
 //Programmer: Steven 
-// Assignment: Ch3 Challenge 1
+// Assignment: Ch3 Challenge 6
 //Filename: main.cpp
 //Creation Date: 10/06/16
-//Description: The purpose of this program is to calculate gas mileage for a car. 
-//Book description: Write a program that calculates a car's gas mileage. The program should ask for. 
-// The number of gallons of gas the car can hold, the number of miles driven on a full tank. 
-// It should display the number of miles driven per gallon. Or miles per gallon. 
+//Description: How many widgets?
+//Book description: 
 
+/*
+
+The Yukon Widget Company manufactures widgets that weigh 9.2 pounds each.
+Write a program that calculates how many widgets are stacked on a pallet, based on
+the total weight of the pallet. The program should ask the user how much the pallet
+weighs by itself and with the widgets stacked on it. It should then calculate and display
+the number of widgets stacked on the pallet.
+
+*/
 //Ch3, challenge 1, Miles per Gallon. 
 
 //Preprocessor Directive 
-#include <iostream>$
+#include <iostream>
+#include "string.h"
+#include <sstream>
 using namespace std;
+
+int main() {
+	double widget, pallet; //variable declaration
+	widget = 9.2; // in pounds
+
+	cout << "How much does the pallet weigh on its own: ";
+	cin >> pallet;
+	cout << "The total weight of the pallet and widget is: " << pallet + widget << endl;
+
+	return 0;
+}
+
