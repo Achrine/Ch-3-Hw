a9d2419f38208da2549fff3e17caa77a1cf4923d
mvc <mvc>
2016-10-11 16:32:46 -0700

    Problem 2. Finished. Three more to go

--- Ch3-2/main.cpp
+++ Ch3-2/main.cpp
@@ -1,14 +1,64 @@
 //Programmer: Steven 
-// Assignment: Ch3 Challenge 1
+// Assignment: Ch3 Challenge 2
 //Filename: main.cpp
 //Creation Date: 10/06/16
-//Description: The purpose of this program is to calculate gas mileage for a car. 
-//Book description: Write a program that calculates a car's gas mileage. The program should ask for. 
-// The number of gallons of gas the car can hold, the number of miles driven on a full tank. 
-// It should display the number of miles driven per gallon. Or miles per gallon. 
+//Description: Stadium Seating 
+//Book description: 
+/*
 
-//Ch3, challenge 1, Miles per Gallon. 
+There are three seating categories at a stadium. For a softball game, Class A seats cost
+$15, Class B seats cost $12, and Class C seats cost $9. Write a program that asks how
+many tickets for each class of seats were sold, then displays the amount of income
+generated from ticket sales. Format your dollar amount in xed-point notation, with
+two decimal places of precision, and be sure the decimal point is always displayed.
+
+*/
+//Ch3, challenge 2, Stadium Seating. 
 
 //Preprocessor Directive 
-#include <iostream>$
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include "string.h"
+
+using namespace std; //call this portion of the libraries called 
+
+int main() { //worlds greatest function in existence. It does it all. 
+	int seatsA, seatsB, seatsC, sTotal; // variable declaration 
+		seatsA = 15,
+		seatsB = 12,
+		seatsC = 9;
+		sTotal = seatsA + seatsB + seatsC;
+		
+	int ticketA = 0, ticketB = 0, ticketC = 0;
+		
+
+	cout << "How many tickets did Class A buy: ";
+	cin >> ticketA;
+	cout << "\nHow many tickets did Class B buy: ";
+	cin >> ticketB;
+	cout << "\nHow many tickets did Class C buy: ";
+	cin >> ticketC;
+
+	
+
+	//Processing 
+	
+	double ind_revA = seatsA * ticketA,
+		ind_revB = seatsB * ticketB,
+		ind_revC = seatsC * ticketC,
+		tTotal = ticketA + ticketB + ticketC,
+		tRevenue = tTotal * sTotal;
+
+	
+
+
+	cout << "Revenue generated from Classes\n";
+	cout << setprecision(2) << fixed; //set precision
+	cout << "Class A: " << setw(10) << ind_revA << endl; //formatting 
+	cout << "Class B: " << setw(10) << ind_revB << endl;
+	cout << "Class C: " << setw(10) << ind_revC << endl;
+	cout << "Class Total " << setw(10) << tRevenue << endl;
+
+	
+	return 0;
+}
