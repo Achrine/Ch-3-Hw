8b677fafb9d2f15a1dc385b4b5790c830d2826af
Steven Brcak <achrine1@gmail.com>
2016-10-13 09:58:10 -0700

    Problem 11, finished

--- Ch3-11/main.cpp
+++ Ch3-11/main.cpp
@@ -1,14 +1,48 @@
 //Programmer: Steven 
-// Assignment: Ch3 Challenge 1
+// Assignment: Ch3 Challenge 11
 //Filename: main.cpp
 //Creation Date: 10/06/16
-//Description: The purpose of this program is to calculate gas mileage for a car. 
-//Book description: Write a program that calculates a car's gas mileage. The program should ask for. 
-// The number of gallons of gas the car can hold, the number of miles driven on a full tank. 
-// It should display the number of miles driven per gallon. Or miles per gallon. 
+//Description: Convert USD to Euro and Yen
+//Book description:
+/*
+ 
+ 
+ Write a program that will convert U.S. dollar amounts to Japanese yen and to euros, storing the conversion factors in the constants 
+ YEN PER DOLLAR and EUROS PER DOLLAR. 
+ To get the most up-to-date exchange rates, search the Internet using the term
+ 
+ "currency exchange rate". If you cannot find the most recent exchange rates, use the following:
+ 1 Dollar = 83.14 Yen
+ 1 Dollar = 0.7337 Euros
+ 
+ Format your currency amounts in fixed-point notation, with two decimal places of precision, and be sure the decimal point is always displayed.
+ 
+*/
 
-//Ch3, challenge 1, Miles per Gallon. 
+//Ch3, challenge 1, Miles per Gallon.
 
 //Preprocessor Directive 
-#include <iostream>$
-using namespace std;
+#include <iostream> //for standard stuff
+#include <sstream>
+#include <iomanip> //for formatting
+using namespace std; //so I don't have to keep putting std:: everywhere
+
+int main(){ //main function, returning an integer
+    //Variable Declaration;
+    //USD can double as the amount had.
+    double USD = 1.00, yen_per_dollar = USD * 83.14, euro_per_dollar = USD * 0.7337;
+    
+    cout << "How much American money do you have: ";
+    cin >> USD;
+    
+    //Processing and Printing
+    cout << setprecision(2) << fixed; //set precision
+    cout << "Money in Yen and Euro\n";
+    cout << "_________________________\n";
+    cout << "Yen per dollar  : "  << right << setw(5) << yen_per_dollar << endl; //print the variable
+    cout << "Euro per dollar : " << right << setw(5) << euro_per_dollar << endl;//print the variable
+    
+    
+    
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 Ch 3 Hw for Gaddis Starting out with C++
 
 
-Problems 1,3,4,5,6,7 and 10 are already done. Go bottom up
+Problems 1,2,3,4,5,6,7,10 and 11 are already done. Go bottom up
 
+Teacher ones: 1,3,4,10
 
+Mine: 2,5,6,7,11
 
 Source code, object code, executable code. Essay question on test. Ch 1
